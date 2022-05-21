f8d44a48da4f19bfe72ac952730f06c313485385
Mukomol32 <91371944+Mukomol32@users.noreply.github.com>
2022-05-21 21:27:58 +0300

    Initial commit

---
+++ src/odessa/Cones.java
@@ -0,0 +1,12 @@
+package odessa;
+
+import java.util.ArrayList;
+
+public class Cones {
+    public ArrayList<Circle> list;
+
+    public Cones() {
+        this.list = new ArrayList<>();
+    }
+
+}
